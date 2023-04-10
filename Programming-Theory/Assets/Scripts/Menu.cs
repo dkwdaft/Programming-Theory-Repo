e108831f56b978a964baf1816c964f57f3ecf16c
dkwdaft <31158132+dkwdaft@users.noreply.github.com>
2023-04-10 09:25:40 +0100

    Complete menu system

---
+++ Programming-Theory/Assets/Scripts/Menu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+   public void Play()
+   {
+       SceneManager.LoadScene(1);
+   }
+
+    
+   public void Quit()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
